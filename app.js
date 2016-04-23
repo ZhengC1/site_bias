57b646e1f3aae41fb881c064244520b8497fc202
camwood23 <camwood23@gmail.com>
2016-04-22 20:31:45 -0400

    I moved stuff to the routes.js

--- app.js
+++ app.js
@@ -10,29 +10,6 @@ var users = require('./routes/users');
 
 var app = express();
 
-var Twitter = require('twitter');
-
-var client = new Twitter({
-  consumer_key: 'yn2L4C7dpYzpU5g2ajm5w1JkX',
-  consumer_secret: 'b4w0nV4W2NHQ2lb4yc2z3uTKD243UiKnXbNRwaKNWVYm35DV07',
-  access_token_key: '723647766707245056-rH6tIGlC5xJiWXRBKaYRzXSJG9cKnhp',
-  access_token_secret: 'S5ZL9cMCazTwbGeV6hGdpkmlbuIagkLqk1trP9N2zYxgR'
-});
-
-var params = {screen_name: 'apphack7'};
-//username='apphack7';
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if(error) console.log(error);
-  console.log(tweets.length);
-});
-
-//client.post('statuses/update', {status: 'Chun is cool'}, function(error, tweets, res) {
-  //if(error) {
-    //throw error;
-  //}
-  //console.log(res);
-//});
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
