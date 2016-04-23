517d9c163954eb758f969395ad97e1f52be5661d
ZhengC1 <zhengc1@appstate.edu>
2016-04-22 23:57:36 -0400

    this is also not working

--- routes/index.js
+++ routes/index.js
@@ -17,10 +17,14 @@ router.get('/', function(req, res, next) {
 
 router.post('/twitter', function(req, res, next) {
   var params = { screen_name: req.body.screen_name };
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if(error) console.log(error);
-    res.render('index', { title: 'Twitter Political Scraper', info: tweets });
-  });
+   client.stream('statuses/filter', {track: params}, function(stream) {
+      stream.on('data', function(data){
+          console.log(params.text);
+      });
+      stream.on('error', function(error){
+          console.log(error);
+      });
+    });
 });
 
 module.exports = router;
