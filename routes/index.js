9b0035c5d45088ab2eb1331e5ae87843c25b028c
camwood23 <camwood23@gmail.com>
2016-04-23 02:18:48 -0400

    Changed iteration... Still slow

--- routes/index.js
+++ routes/index.js
@@ -12,37 +12,35 @@ var client = new Twitter({
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(process.env.positive);
-  console.log(process.env.negative);
   res.render('index', { title: 'Twitter Political Scraper', info: []});
 });
 
 router.post('/searchname', function(req,res) {
-  var params = { screen_name: req.body.screen_name, count: 100 };
+  var params = { screen_name: req.body.screen_name, count: 1 };
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    var tweetArr = new Array();
-    var neg_count = 0
-    var pos_count = 0
+    var neg_count = 0;
+    var pos_count = 0;
+    console.log(process.env.positive.length);
+    console.log(process.env.negative.length);
     for(var i = 0; i < tweets.length; i++)
     {
-        var line = tweets[i].text.split(" ");
-        for(j = 0; j < line.length; j++)
-        {
-            if(line[j] in process.env.positive)
-            {
-                pos_count++;
+      var line = tweets[i].text.split(" ");
+      for(j = 0; j < line.length; j++)
+      {
+        for (q=0;q<process.env.negative.length;q++) {
+          if (q<process.env.positive.length) {
+            if (line[j]===process.env.positive[q]) {
+              pos_count++;
+              break;
             }
+          }
+          if (line[j]===process.env.negative[q]) {
+            neg_count++;
+            break;
+          }
         }
-        for(j = 0; j < line.length; j++)
-        {
-            if(line[j] in process.env.negative)
-            {
-                neg_count++;
-            }
-        }
-        tweetArr.push((tweets[i].text).split(" "));
+      }
     }
-    console.log(tweetArr);
     console.log(pos_count);
     console.log(neg_count);
     res.render('index', { title: 'Tweets', info: tweets});
